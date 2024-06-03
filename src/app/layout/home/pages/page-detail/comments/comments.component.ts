4c60ef42ba90f2af9dd9c1ef77696efaac1369bb
Dev_Snake <dangvanhaufpt2019@gmail.com>
2024-06-03 17:42:21 +0700

    feat : pagination feature

--- src/app/layout/home/pages/page-detail/comments/comments.component.ts
+++ src/app/layout/home/pages/page-detail/comments/comments.component.ts
@@ -1,6 +1,12 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { ApiService } from '../../../../../shared/service/api/api.service';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
 import {
   FormControl,
   FormGroup,
@@ -10,6 +16,7 @@ import {
 import { Products } from '../../../../../shared/interfaces/product';
 import { AuthService } from '../../../../../shared/service/auth/auth.service';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-comments',
   standalone: true,
@@ -17,12 +24,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.css',
 })
-export class CommentsComponent {
+export class CommentsComponent implements OnChanges {
   @Output() comment = new EventEmitter<string>();
   @Input() commentList: any[] = [];
   @Input() isLogged: boolean = false;
   @Input() product: Products[] | undefined = [];
   public formComment: FormGroup;
+  public displayedComments: any[] = [];
+  public pageSize: number = 3;
+  public currentPage: number = 1;
+
   constructor(private authService: AuthService) {
     const day = new Date();
     const date = day.getDate();
@@ -31,14 +42,31 @@ export class CommentsComponent {
     const time = day.getHours();
     const minute = day.getMinutes();
     const second = day.getSeconds();
-    const fullTime = `${date}/${month}/${year} ${time}:0${minute}:${second}`;
+    const fullTime = `${date}/${month}/${year} ${time}:${minute}:${second}`;
     this.formComment = new FormGroup({
       username_customer: new FormControl(this.authService.getUsername()),
       content: new FormControl('', [Validators.required]),
       date: new FormControl(fullTime),
     });
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['commentList']) {
+      this.loadComments();
+    }
+  }
+
   addComment() {
     this.comment.emit(this.formComment.value.content);
+  }
+
+  loadComments() {
+    const endIndex = this.currentPage * this.pageSize;
+    this.displayedComments = this.commentList.slice(0, endIndex);
+  }
+
+  showMoreComments() {
+    this.currentPage++;
+    this.loadComments();
   }
 }
